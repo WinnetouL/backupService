86a22403c38dfcf9dae933e41ee2c05556350f17
t3 <tf.hamburg@web.de>
2020-02-05 22:48:19 +0100

    added a process of choosing the destination volume to run the backup

--- backup.ps1
+++ backup.ps1
@@ -1,20 +1,27 @@
-﻿Write-Host "Setup phase"
+﻿# init required variabels
+$saveFolder = New-Object System.Collections.Generic.List[string]
+$destVolPath="NONE"
+
+
+Write-Host "Setup phase"
 Write-Host "-----------"
+
 # choose backup type
 Write-Host "Which kind of backup do you want to execute (enter 1 or 2)"
 Write-Host "1: Entire new backup"
 Write-Host "2: Update an existing backup (incremental)"
 $backupType = Read-Host
-Switch($backupType) {
-    1{$type = 'Die Variable ist 1'}
-    2{$type = 'Die Variable ist 2'}
-    default {Write-Host "exit"; exit}
-}
-$type
 
-# init required variabels
-$saveFolder = New-Object System.Collections.Generic.List[string]
-$destinationFolder
+# backup location
+$possibleDestVolumes += Get-WMIObject win32_volume -Filter "DriveType='2'" | Select-Object -property Label, Name # 2 = Removable
+Write-Host $possibleDestVolumes.Label
+$destVolName = Read-Host "Enter your destination volume "
+$a | % { if( $_.Label -ceq $destVolName){$destVolPath=$_.Name}} # % = foreach; -ceq -> case sensitive
+Write-Host $destVolPath
+
+
+
+
 
 # add the folders which need to be backed up to list
 do{
@@ -33,7 +40,7 @@ do{
 function checkIfPathExist($path) {
     $addOrNot = Test-Path -Path $path
     return $addOrNot
-} 
+}
 
 Write-Host "The following folders are going to be backed up:"
 for ($i=0; $i -lt $saveFolder.length; $i++){
