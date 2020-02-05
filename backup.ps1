c60e24cce9c5af56f13ae03e4d36ac1d0d46c227
t3 <tf.hamburg@web.de>
2020-02-05 08:10:12 +0100

    added a choice of the type of update you want to run

--- backup.ps1
+++ backup.ps1
@@ -1,6 +1,22 @@
 ﻿Write-Host "Setup phase"
 Write-Host "-----------"
 
+do{
+    Write-Host "Which kind of backup do you want to execute (enter 1 or 2)"
+    Write-Host "1: Entire new backup"
+    Write-Host "2: Update an existing backup (incremental)"
+    $updateType = Read-Host
+} while (($updateType -eq "1") -or ($updateType -eq "2"))
+
+
+Switch($Eingabe) {
+    0{$Detail = 'Die Variable ist 0'}
+    1{$Detail = 'Die Variable ist 1'}
+    2{$Detail = 'Die Variable ist 2'}
+    3{$Detail = 'Die Variable ist 3'}
+    4{$Detail = 'Die Variable ist 4'}
+}
+$Detail
 # init required variabels
 $saveFolder = New-Object System.Collections.Generic.List[string]
 $destinationFolder
