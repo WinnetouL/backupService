5b5b8a2454de1a2cc74fc3b49649c61cf557eab8
t3 <tf.hamburg@web.de>
2020-02-06 08:37:37 +0100

    adjusted configuration part and added a confirmation section

--- backup.ps1
+++ backup.ps1
@@ -7,21 +7,21 @@ Write-Host "Setup phase"
 Write-Host "-----------"
 
 # choose backup type
-Write-Host "Which kind of backup do you want to execute (enter 1 or 2)"
-Write-Host "1: Entire new backup"
-Write-Host "2: Update an existing backup (incremental)"
-$backupType = Read-Host
+Write-Host "Available backup types:"
+Write-Host "    1: Entire new backup"
+Write-Host "    2: Update an existing backup (incremental)"
+$backupType = Read-Host "Which kind of backup do you want to execute (enter 1 or 2)"
 
 # backup location
-$possibleDestVolumes += Get-WMIObject win32_volume -Filter "DriveType='2'" | Select-Object -property Label, Name # 2 = Removable
-Write-Host $possibleDestVolumes.Label
+$possibleDestVolumes = Get-WMIObject win32_volume | Select-Object -property Label, Name # 2 = Removable -Filter "DriveType='2'"
+$possibleDestVolumesOut = $possibleDestVolumes | select -ExpandProperty Label
+$_=1
+for ($i=0; $i -lt $possibleDestVolumesOut.length; $i++){
+    Write-Host $_ "-" $possibleDestVolumesOut[$i]
+    $_++
+}
 $destVolName = Read-Host "Enter your destination volume "
-$a | % { if( $_.Label -ceq $destVolName){$destVolPath=$_.Name}} # % = foreach; -ceq -> case sensitive
-Write-Host $destVolPath
-
-
-
-
+$possibleDestVolumes | % { if( $_.Label -ceq $destVolName){$destVolPath=$_.Name}} # % = foreach; -ceq -> case sensitive
 
 # add the folders which need to be backed up to list
 do{
@@ -42,15 +42,25 @@ function checkIfPathExist($path) {
     return $addOrNot
 }
 
+# confirmation
+Write-Host "Selected backup type:"
+switch ($backupType){
+1 {"    -> Backup Type - New backup"}
+2 {"    -> Backup Type - Incremental Backup"}
+default {"    Error: None existing backup type - exit!"; exit}
+}
 Write-Host "The following folders are going to be backed up:"
 for ($i=0; $i -lt $saveFolder.length; $i++){
-    Write-Host "- $saveFolder[$i]"
+    Write-Host "    -> $saveFolder[$i]"
 }
-$confirmation = Read-Host -Prompt "Hit 'y' to continue"
-if ($confirmation -eq "y") {
+Write-Host "Selected Volume:"
+Write-Host "    ->" $destVolPath
+
+$confirmation = Read-Host -Prompt "Write 'Yes' to confirm or 'no' to cancel"
+if ($confirmation -ceq "Yes") {
     continue
 }Else {
-    Write-Host ""
+    Write-Host "    -> Canceled!"
 }
 
 
