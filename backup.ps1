a93f3a87928a4b50cb3a03d2937e004632eaab2f
t3 <tf.hamburg@web.de>
2020-02-10 16:27:09 +0100

    moved functions in a separate file and included it; tweaked the output

--- backup.ps1
+++ backup.ps1
@@ -1,57 +1,4 @@
-﻿# check if variable is a existing path
-function checkIfPathExist($path) { # error - when pressing enter
-    $addOrNot = Test-Path -Path $path
-    return $addOrNot
-    }
-# returns an array with the required paths
-function highestNumDirName($itemList) {
-    $backupNames = New-Object System.Collections.Generic.List[string]
-    $highestNumBackup = 0
-    for ($i=0; $i -lt $itemList.length; $i++){
-        if ($highestNumBackup -lt ($itemList[$i].Name -as [int])) {
-            $highestNumBackup = $itemList[$i].Name -as [int]
-            }
-        $secHighestPathBackup = $itemList[$i].FullName # better choose modification date 
-        }
-    $firstHighestNumBackup = $highestNumBackup + 1 | % {"{0:d3}" -f $_}
-    $firstHighestPathBackup = Join-Path -Path $destVolPath -ChildPath $firstHighestNumBackup
-    $backupNames += $firsthighestPathBackup
-    $backupNames += $secHighestPathBackup
-    return $backupNames
-    }
-# create directories silently
-function makeDir($path) {
-    New-Item -Path $path -type Directory | Out-Null # "> $null" would be much faster
-    }
-# remove directories silently
-function removeDir($path) {
-    Remove-Item -path $path -Recurse -Force
-    }
-# returns a list of all childpathes of directories inside a parent path
-function genListSubDir($sourPath) {
-    $listOfDir = New-Object System.Collections.Generic.List[string]
-    for ($i=0; $i -lt $sourPath.Count; $i++){
-        Get-ChildItem -Path $sourPath[$i] -Recurse -Force -Attributes Directory | % {$listOfDir.Add($_.FullName)}
-        }
-    return $listOfDir
-    }
-# returns a list wish hash and filenames
-function calcHash($path) {
-    $hashList = New-Object System.Collections.Generic.List[string]
-    Get-ChildItem -Path $path -Force | % {$hashList.Add($_.FullName); Get-FileHash $_.FullName -Algorithm SHA1} | % {$hashList.Add($_.Hash)}
-    return $hashList
-    }
-
-# copy files
-function copyFile($sourcePath, $backupQualifier, $backupType) {
-    if ($backupType -eq "1") {
-        $destPath = Split-Path -Path $sourcePath -NoQualifier | % {Join-Path -Path $backupQualifier -ChildPath $_}
-        Copy-Item -Path $sourcePath -Destination $destPath -Force -Recurse
-    }elseif ($backupType -eq "2") {
-        $destPath = Split-Path -Path $sourcePath -NoQualifier | % {Join-Path -Path $backupQualifier -ChildPath $_}
-        Copy-Item -Path $sourcePath -Destination $destPath -Force
-        }
-    }
+﻿. "$PSScriptRoot\funcOfBackupSer.ps1"
 
 # init required variabels
 $sourFilePath = New-Object System.Collections.Generic.List[string]
@@ -128,13 +75,15 @@ $itemsBackupEnv = Get-ChildItem -Path $destVolPath -Attributes Directory
 $backupNames = highestNumDirName $itemsBackupEnv
 $backupNames = [System.Collections.Generic.List[string]]@($backupNames) # object(array) -> list(object)
 if ($backupType -eq "1") {
-    Write-Host "1 ---" $backupNames[0]
     makeDir $backupNames[0]
+    Write-Host "`nCopied Directories:"
     for ($i=0; $i -lt $sourFilePath.Count; $i++){
         Write-host $i "--Copy--" $sourFilePath[$i]
         copyFile $sourFilePath[$i] $backupNames[0] $backupType
     }
+    Write-Host "`nNew Backup at: " $backupNames[0]
 }elseif ($backupType -eq "2") {
+
     # get a list of subdirs at destination
     $backupNames.RemoveAt(0) # QuickFix: need to adjust the list due I want just a specific path for next step and my function doesn't work with '$backupNames[1]'
     $listExistDestDir = genListSubDir $backupNames # get a list of the dir which already exist, to be able to determine which dirs can be deleted
@@ -172,6 +121,7 @@ if ($backupType -eq "1") {
     foreach ($dir in $listRmDir) {removeDir $dir}
     
     # create subdir at destination location if it doesn't exist
+    Write-Host "`nFolder synchronization:"
     for ($i=0; $i -lt $listOfDestDir.Count; $i++){
         if (!(checkIfPathExist $listOfDestDir[$i])) {
             makeDir $listOfDestDir[$i]
@@ -180,6 +130,7 @@ if ($backupType -eq "1") {
                 Write-Host "No new folder created at destination!"
             }
         }
+
     # get list of files to copy
     $filesToCopy = New-Object System.Collections.Generic.List[string]
     for ($i=0; $i -lt $listOfSourDir.Count; $i++){
@@ -197,8 +148,10 @@ if ($backupType -eq "1") {
           
             }
     }
+    Write-Host "`nFile synchronization:"
     for ($i=0; $i -lt $filesToCopy.Count; $i++){
         Write-host $i "--Copy--" $filesToCopy[$i]
         copyFile $filesToCopy[$i] $backupNames[0] $backupType
         }
+    Write-Host "`nUpdated the following Backup: " $backupNames[0]
     }
