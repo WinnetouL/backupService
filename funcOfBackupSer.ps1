ad04e04b689b079638fd18b4237d2d805282561e
T3 <50875993+WinnetouL@users.noreply.github.com>
2020-02-17 20:30:14 +0100

    return string instead of array; renamed variables and removed unused function

--- funcOfBackupSer.ps1
+++ funcOfBackupSer.ps1
@@ -4,21 +4,23 @@ function checkPathExist($path) {
     return $addOrNot
     }
 
-# returns an array with the required paths
-function highestNumDirName($itemList, $envPath) {
-    $backupNames = New-Object System.Collections.Generic.List[string]
+# returns a string with the corresponding backup path
+function getBackupName($itemList, $envPath, $backupType) {
     $highestNum = 0
     for ($i=0; $i -lt $itemList.length; $i++){
         if ($highestNum -lt ($itemList[$i].Name -as [int])) {
             $highestNum = $itemList[$i].Name -as [int]
             }
-        $secHighestPath = $itemList[$i].FullName # better choose modification date 
+        $highestNumPath = $itemList[$i].FullName # better choose modification date 
         }
-    $firstHighestNum = $highestNum + 1 | ForEach-Object {"{0:d3}" -f $_}
-    $firstHighestPath = Join-Path -Path $envPath -ChildPath $firstHighestNum
-    $backupNames += $firsthighestPath
-    $backupNames += $secHighestPath
-    return $backupNames
+    if ($backupType -eq "1") {
+        $newHighestNum = $highestNum + 1 | ForEach-Object {"{0:d3}" -f $_}
+        $newHighestNumPath = Join-Path -Path $envPath -ChildPath $newHighestNum
+        return $newHighestNumPath
+        }
+    elseif ($backupType -eq "2") {
+        return $highestNumPath
+        }
     }
 
 # create directories silently
@@ -30,15 +32,6 @@ function makeDir($path) {
 function removeDir($path) {
     Write-Host "`t-> --Remove--" $path
     Remove-Item -path $path -Recurse -Force
-    }
-
-# returns a list of all childpathes of directories inside a parent path
-function genListSubDir($sourPath) {
-    $dirs = New-Object System.Collections.Generic.List[string]
-    for ($i=0; $i -lt $sourPath.Count; $i++){
-        Get-ChildItem -Path $sourPath[$i] -Recurse -Force -Attributes D | ForEach-Object {$dirs.Add($_.FullName)}
-        }
-    return $dirs
     }
 
 # returns a list wish hash and filenames
